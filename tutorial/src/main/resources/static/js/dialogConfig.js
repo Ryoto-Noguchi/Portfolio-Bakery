7ce5de7834ebb71db734479e57c577da4994b6fa
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2020-12-17 22:39:55 +0900

    新規宛先登録確認モーダルまで完了

--- tutorial/src/main/resources/static/js/dialogConfig.js
+++ tutorial/src/main/resources/static/js/dialogConfig.js
@@ -12,43 +12,43 @@ const dialogConfig = {
 			},
 		]
 	},
-	inputDestinationConfirm: {
-		autoOpen:false,
-		width: 850,
-		modal: true,
-		buttons: [
-			{
-				text: '登録',
-				click: function() {
-					let jsonString = {
-						'familyName': $('#familyName'),
-						'firstName': $('#firstName'),
-						'address': $('#address'),
-						'telNumber': $('telNumber')
-					};
-					$.ajax({
-						type: 'POST',
-						url: 'tutorial/destination/register',
-						data: JSON.stringify(jsonString),
-						contentType: 'application/json',
-						dataType: 'json',
-					}).done(function(data) {
-						alert('新規宛先を登録しました');
-						location.href='/tutorial/purchase';
-					}).fail(function(data) {
-						alert('ajax通信に失敗しました');
-					}).always(function(data) {
-						console.log('ajax通信しました');
-					});
-					$(this).dialog('close');
-				},
-			},
-			{
-				text: '戻って修正',
-				click: function() {
-					$(this).dialog('close');
-				}
-			},
-		]
-	},
+	// inputDestinationConfirm: {
+	// 	autoOpen:false,
+	// 	width: 850,
+	// 	modal: true,
+	// 	buttons: [
+	// 		{
+	// 			text: '登録',
+	// 			click: function() {
+	// 				let jsonString = {
+	// 					'familyName': $('#familyName'),
+	// 					'firstName': $('#firstName'),
+	// 					'address': $('#address'),
+	// 					'telNumber': $('telNumber')
+	// 				};
+	// 				$.ajax({
+	// 					type: 'POST',
+	// 					url: 'tutorial/destination/register',
+	// 					data: JSON.stringify(jsonString),
+	// 					contentType: 'application/json',
+	// 					dataType: 'json',
+	// 				}).done(function(data) {
+	// 					alert('新規宛先を登録しました');
+	// 					location.href='/tutorial/purchase';
+	// 				}).fail(function(data) {
+	// 					alert('ajax通信に失敗しました');
+	// 				}).always(function(data) {
+	// 					console.log('ajax通信しました');
+	// 				});
+	// 				$(this).dialog('close');
+	// 			},
+	// 		},
+	// 		{
+	// 			text: '戻って修正',
+	// 			click: function() {
+	// 				$(this).dialog('close');
+	// 			}
+	// 		},
+	// 	]
+	// },
 };
